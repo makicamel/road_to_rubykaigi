f8311c2762b71c6b1d2bf0511cc8d1baf07b637d
makicamel <unright@gmail.com>
2025-02-15 21:40:12 +0900

    Use 2 spaces

--- lib/road_to_rubykaigi/map.rb
+++ lib/road_to_rubykaigi/map.rb
@@ -1,31 +1,31 @@
 module RoadToRubykaigi
-    class Map
-      attr_reader :width, :height
+  class Map
+    attr_reader :width, :height
 
-      def initialize
-        @map = [
-          "####################################################",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "#                                                  #",
-          "####################################################",
-        ]
-        @height = @map.size
-        @width  = @map.first.size
-      end
+    def initialize
+      @map = [
+        "####################################################",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "#                                                  #",
+        "####################################################",
+      ]
+      @height = @map.size
+      @width  = @map.first.size
+    end
 
-      def draw
-        str = @map.map.with_index do |line, i|
-          "\e[#{i+1};1H" + line
-        end.join("\n")
-        puts str
-      end
+    def draw
+      str = @map.map.with_index do |line, i|
+        "\e[#{i+1};1H" + line
+      end.join("\n")
+      puts str
     end
   end
+end
--- lib/road_to_rubykaigi/player.rb
+++ lib/road_to_rubykaigi/player.rb
@@ -1,59 +1,59 @@
 module RoadToRubykaigi
-    class Player
-      DELAY = 0.6
-      FRAMES = [
-        [
-          "╭──────╮ ",
-          "│｡・◡・│_◢◤",
-          "╰ᜊ───ᜊ─╯"
-        ],
-        [
-          "╭──────╮ ",
-          "│｡・◡・│_◢◤",
-          "╰─∪───∪╯ "
-        ],
-      ]
+  class Player
+    DELAY = 0.6
+    FRAMES = [
+      [
+        "╭──────╮ ",
+        "│｡・◡・│_◢◤",
+        "╰ᜊ───ᜊ─╯"
+      ],
+      [
+        "╭──────╮ ",
+        "│｡・◡・│_◢◤",
+        "╰─∪───∪╯ "
+      ],
+    ]
 
-      def move(dx, dy, map_width, map_height)
-        new_x = @x + dx
-        new_y = @y + dy
-        @x = clamp(new_x, 2, map_width - width)
-        @y = clamp(new_y, 2, map_height - height)
-      end
+    def move(dx, dy, map_width, map_height)
+      new_x = @x + dx
+      new_y = @y + dy
+      @x = clamp(new_x, 2, map_width - width)
+      @y = clamp(new_y, 2, map_height - height)
+    end
 
-      def draw
-        str = FRAMES[@frame_index].map.with_index do |line, i|
-          "\e[#{@y+i};#{@x}H" + line
-        end.join("\n")
-        print str
-      end
+    def draw
+      str = FRAMES[@frame_index].map.with_index do |line, i|
+        "\e[#{@y+i};#{@x}H" + line
+      end.join("\n")
+      print str
+    end
 
-      def update_frame
-        if (Time.now - @frame_last_update) >= DELAY
-          @frame_index = (@frame_index + 1) % FRAMES.size
-          @frame_last_update = Time.now
-        end
-      end
-
-      private
-
-      def initialize(x = 10, y = 9)
-        @x = x
-        @y = y
-        @frame_index = 0
+    def update_frame
+      if (Time.now - @frame_last_update) >= DELAY
+        @frame_index = (@frame_index + 1) % FRAMES.size
         @frame_last_update = Time.now
       end
+    end
 
-      def width
-        @width ||= FRAMES.first.map(&:size).max
-      end
+    private
 
-      def height
-        @height ||= FRAMES.map(&:size).max
-      end
+    def initialize(x = 10, y = 9)
+      @x = x
+      @y = y
+      @frame_index = 0
+      @frame_last_update = Time.now
+    end
 
-      def clamp(v, min, max)
-        [[v, min].max, max].min
-      end
+    def width
+      @width ||= FRAMES.first.map(&:size).max
+    end
+
+    def height
+      @height ||= FRAMES.map(&:size).max
+    end
+
+    def clamp(v, min, max)
+      [[v, min].max, max].min
     end
   end
+end
