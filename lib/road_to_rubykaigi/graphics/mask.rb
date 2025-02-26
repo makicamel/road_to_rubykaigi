400b082cbd73c3283b1b7abe3e2cf8308c311286
makicamel <unright@gmail.com>
2025-02-26 21:21:38 +0900

    Note down the characters to generate the mask
    (Mask generation is handcraft)

--- lib/road_to_rubykaigi/graphics/mask.rb
+++ lib/road_to_rubykaigi/graphics/mask.rb
@@ -2,7 +2,7 @@ module RoadToRubykaigi
   module Graphics
     module Mask
       FILE_PATH = "mask.txt"
-
+      MASK_CHARAS = "[╭─╮╰│╯┬◯╔═║╠╚╦╝╗╣┤┴┼├╽▐▗▖▌◻▥▞▟█▙◺◸┌┐┘└╨▝▘▄▜▛▀░▓]"
       def self.data
         File.read("#{__dir__}/#{FILE_PATH}").split("\n")
       end
