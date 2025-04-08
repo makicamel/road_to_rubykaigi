31f787327d8ea4bfca419307106f404cd4262497
makicamel <unright@gmail.com>
2025-04-09 06:11:13 +0900

    Change default duty cycle

--- lib/road_to_rubykaigi/audio/oscillator.rb
+++ lib/road_to_rubykaigi/audio/oscillator.rb
@@ -37,13 +37,33 @@ module RoadToRubykaigi
 
     class SquareOscillator
       include Phasor
+      DUTY_CYCLE = {
+        d0: 0.125,
+        d1: 0.25,
+        d2: 0.5,
+      }
 
       def generate(frequencies:)
         samples = frequencies.map do |frequency|
           phase = tick(frequency: frequency)
-          phase < 0.5 ? 1.0 : -1.0
+          phase < duty_cycle ? 1.0 : -1.0
         end
         samples.sum / samples.size
+      end
+
+      def duty_cycle=(duty_cycle)
+        @duty_cycle = (DUTY_CYCLE.key?(duty_cycle) ? duty_cycle : :d0)
+      end
+
+      private
+
+      def initialize
+        @duty_cycle = :d1
+        super
+      end
+
+      def duty_cycle
+        DUTY_CYCLE[@duty_cycle]
       end
     end
   end
