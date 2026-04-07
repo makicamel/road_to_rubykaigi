5d0531e5cf41c63ea0d98f01102d56d20b20bae5
makicamel <unright@gmail.com>
2026-04-07 09:56:45 +0900

    Refactor

--- lib/road_to_rubykaigi/signal_interpreter.rb
+++ lib/road_to_rubykaigi/signal_interpreter.rb
@@ -1,37 +1,45 @@
+require 'singleton'
+
 module RoadToRubykaigi
   class SignalInterpreter
+    include Singleton
+
     class << self
-      def process
-        data = pick
-        return unless data
+      extend Forwardable
+      def_delegators :instance, :process
+    end
 
-        action = interpret(data)
-        EventDispatcher.publish(:input, action) if action
+    def process
+      data = pick
+      return unless data
+
+      if (action = interpret(data))
+        EventDispatcher.publish(:input, action)
       end
+    end
 
-      private
+    private
 
-      def pick
-        return if GameServer.queue.empty?
+    def pick
+      return if GameServer.queue.empty?
 
-        GameServer.queue.pop(true)
-      end
+      GameServer.queue.pop(true)
+    end
 
-      def interpret(data)
-        return unless %w[x y z].all? { |key| data.key?(key) }
+    def interpret(data)
+      return unless %w[x y z].all? { |key| data.key?(key) }
 
-        x = data['x'].to_f
-        y = data['y'].to_f
-        z = data['z'].to_f
+      x = data['x'].to_f
+      y = data['y'].to_f
+      z = data['z'].to_f
 
-        # TODO: implement
-        case
-        when y < -5 then :jump
-        when y > 5 then :crouch
-        when x < -3 then :left
-        when x > 3 then :right
-        when z < -5 then :attack
-        end
+      # TODO: implement
+      case
+      when y < -5 then :jump
+      when y > 5 then :crouch
+      when x < -3 then :left
+      when x > 3 then :right
+      when z < -5 then :attack
       end
     end
   end
